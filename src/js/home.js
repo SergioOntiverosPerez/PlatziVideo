eebac8340105d8036658dd458168c47894582f2b
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-08-03 16:52:28 -0300

    The Form was created

--- src/js/home.js
+++ src/js/home.js
@@ -65,8 +65,23 @@
       $element.setAttribute(attribute, attributes[attribute])
     }
   }
+  const BASE_API = 'https://yts.mx/api/v2/'
 
-  $form.addEventListener('submit', (event) => {
+  function featuringTemplate(peli) {
+    return (
+      `<div class="featuring">
+        <div class="featuring-image">
+          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${peli.title}</p>
+        </div>
+      </div>`
+    )
+  }
+
+  $form.addEventListener('submit', async (event) => {
     // debugger
     event.preventDefault() // para evitar que recargue
     $home.classList.add('search-active')
@@ -77,10 +92,14 @@
       width: 50,
     })
     $featuringContainer.append($loader)
+    const data = new FormData($form)
+    const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+    const HTMLString = featuringTemplate(peli.data.movies[0])
+    $featuringContainer.innerHTML = HTMLString
   })
-  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action')
-  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama')
-  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation')
+  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`)
+  const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`)
+  const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`)
 
 
   // INCLUIR HTML CON JAVASCRIPT  // TEMPLATES
